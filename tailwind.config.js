0e526a060e69414071bcc473b84b1e9a4d8b0164
HelloRuru <vmpvmp1017@gmail.com>
2026-01-28 00:41:58 +0800

    New day!

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'navy-soft': '#6B8CAE',
+        'cream-yellow': '#FFD97D',
+        'pink-orange': '#FFB6A3',
+        'cream-bg': '#FFF9F0',
+      },
+    },
+  },
+  plugins: [],
+}
